a2ec79853d542412ab6ab2e3a6810dbfc924a485
kolinhh <linlinhte1942000@gmail.com>
2023-07-13 13:27:51 +0630

    first version

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+const userRoute = require('./routes/user');
+app.use="/users", userRoute);
+
+const postRoute = require('./routes/post')
+app.use("/posts",postRoute)
+
+
+app.listen(3000, console.log("Server is running at port 3000"));
---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+router.get("/", (req, res) => {
+    res.json({ msg: "All users" })
+});
+router.post("/", (req, res) => {
+    res.json(req.body)
+});
+
+router.route("/:id")
+    .get((req, res) => res.json({ msg: " Request id is " +req.params. id }))
+    .patch((req, res) => res.json({ msg: "Edit id is " + req.params.id }))
+    .delete((req, res) => res.json({ msg: "Delete id is " +req.params. id }))
+
+
+
+
+module.exports = router;
